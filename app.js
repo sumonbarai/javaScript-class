d1581e0105b55f9e1f5d0ab4706414e70b219644
sumonbarai <sumonbarai78@gmail.com>
2025-04-17 00:35:21 +0600

    class-02

---
+++ app.js
@@ -0,0 +1,66 @@
+// Variables are used to store data in JavaScript. For example variable is a box that contains some data
+
+// There are 4 ways to define the variable
+
+// - var
+// - let
+// - const
+// - no identifier / automatically
+
+var sumon = "khulna";
+
+// var sumon;
+
+// sumon = "khulna";
+
+// console.log(sumon);
+
+// let city = "dhaka";
+
+const locations = "purba baniya khamar";
+
+age = 32;
+
+// console.log(age);
+
+// Variable naming convention
+// ======================================
+// - Names can contain letters, digits, underscores, and dollar signs.
+// - variable name's first character must not be a digit
+// - Names can also begin with $ and _  but not use (-) sign
+// - Names are case sensitive (y and Y are different variables).
+// - Reserved words (like JavaScript keywords) cannot be used as names.
+
+// console.log();
+
+/* var myName = "sumon barai";
+var myName = "sumon";
+console.log(myName); */
+
+/* let city = "khulna";
+city = "dhaka";
+city = "pabna";
+
+console.log(city); */
+
+/* const capital = "Dhaka";
+capital = "khulna";
+
+console.log(capital); */
+
+const capital = [1, 5, 6, 9];
+
+/* - `String` - Text values ("Hello")
+- `Number` - Numeric values (25, 3.14)
+- `Boolean` - True/False (true, false)
+- `Undefined` - A variable declared but not assigned (let x;)
+- `Null` - Represents "nothing" (let y = null;)
+- `BigInt` - Large numbers (BigInt(12345678901234567890))
+- `Symbol` - Unique identifiers (Symbol("id"))
+- `Object` - Collection of key-value pairs
+- `Array` - Ordered list of values
+- `function` - Code that can be executed */
+
+const MEMORY = true;
+
+console.log(memory instanceof Boolean);
