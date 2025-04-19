752be9027fcc93e895c8993ad76cdbb124b10a16
sumonbarai <sumonbarai78@gmail.com>
2025-04-19 23:49:25 +0600

    class-3

---
+++ app.js
@@ -0,0 +1,94 @@
+//Operator, Operands, , Expression
+
+// Operator : + ,  = < > </>-
+
+// Operands : 50 + 60  =110
+
+// Expression: An expression is a combination of values, variables, and operators that JavaScript can evaluate and return a single result or value.
+
+// An expression in JavaScript is a piece of code that produces a single value when executed.
+
+// assignment expression  x = 2
+
+// evaluating expression  50 + 60
+
+// Type of Operator
+
+//  1. Arithmetic operator
+//  1. assignment operator
+// 3 comparison operator
+// 3 logical operator
+// 3 relational operator
+// 3 bitwise operator
+// 3 conditional (ternary) operator
+// 3 special operator
+
+/* let x = 10;
+
+let y = 50;
+
+// y = y + x;
+y += x;
+console.log(y); */
+
+/* let x = 10;
+// x++;
+console.log(x);
+// console.log(x); */
+
+// comparison operator
+
+// console.log(50 <= 50);
+
+// logical operator
+
+// and  &&
+// or   ||
+// not   !
+// nullish coalescing  ??
+
+/* 
+js falsy value
+  1 . null
+  2. undefined
+  3. 0
+  4. NaN
+  5. ""
+  6. false
+
+*/
+
+// console.log(undefined ?? "khulna");
+
+// && (and): left side is true then return right side  other wise return left side
+
+// || (or): left side is true, then return left side otherwise return right side
+
+// ! (not) : convert boolean value ( true is false ,false is true )
+
+// - ?? nullish coalescing  left side is null or undefined then return right side other wish return left
+
+// relational operator
+
+//  in
+
+/* const obj = {
+  edu: "akcc",
+};
+
+console.log("edus" in obj);
+ */
+
+// conditional (ternary) operator
+
+/* const result = 50 < 100 ? "akcc" : "chitalmari";
+console.log(result); */
+
+// grouping presidence
+
+console.log((50 + 2) * 3);
+
+// special operator
+
+// typeof
+// instanceof
