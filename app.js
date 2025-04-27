da1864f1739525ae4341bf7400b1fc9349e567ea
sumonbarai <sumonbarai78@gmail.com>
2025-04-27 11:33:19 +0600

    class-04

---
+++ app.js
@@ -0,0 +1,107 @@
+/* 
+if
+if else
+if else if , else
+switch case 
+ternary operator 
+
+break
+continue
+*/
+
+// console.log("cls");
+
+// console.log("hello");
+
+// if (true) {
+//   console.log("world");
+//   console.log(500);
+// }
+
+// if (true) console.log("world");
+
+/* if (false) {
+  console.log("cls");
+  console.log("hello");
+} else {
+  console.log("world");
+  console.log(500);
+}
+ */
+/* let age = 20;
+
+if (age < 18) {
+  console.log("not adult");
+} else {
+  console.log("adult");
+}
+ */
+
+let age = 30;
+
+/* if (age === 20) {
+  console.log("your age is 20");
+} else if (age === 30) {
+  console.log("your age is 30");
+} else if (age === 40) {
+  console.log("your age is 40");
+} else {
+  console.log("your age not valid");
+}
+ */
+
+// same
+/* if (age === 20) {
+  console.log("your age is 20");
+}
+
+if (age === 30) {
+  console.log("your age is 30");
+}
+
+if (age === 40) {
+  console.log("your age is 40");
+}
+ */
+// console.log("you");
+
+/* const marks = 1000;
+
+if (marks >= 0 && marks <= 100) {
+  if (marks >= 80) {
+    console.log("A+");
+  } else if (marks >= 70) {
+    console.log("A");
+  }
+} else {
+  console.log("not valid");
+}
+ */
+
+/* let position = 56;
+
+switch (position) {
+  case 1: {
+    console.log("position 1");
+    console.log("ddd");
+    break;
+  }
+
+  case 2: {
+    console.log("position 2");
+    break;
+  }
+
+  case 3: {
+    console.log("position 3");
+    break;
+  }
+
+  default: {
+    console.log("no position");
+  }
+} */
+
+const result = 50;
+
+result > 20 ? console.log("pass marks") : console.log("fail marks");
